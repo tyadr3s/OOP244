68c2799ff5aa7dd5b0e586d84118c72966c9e702
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-01-14 09:04:48 -0500

    Add Workshop Zero program

---
+++ w0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "#### Workshop Zero ####" << endl;
+    cout << "Subject and Section:" << endl;
+    cout << "OOP244 NAA" << endl;
+    cout << "Name:" << endl;
+    cout << "Carlos Andres Ramirez Moreno" << endl;
+    cout << "Student Number:" << endl;
+    cout << "120847256" << endl;
+    cout << "Email:" << endl;
+    cout << "caramirez-morerno@myseneca.ca" << endl;
+    return 0;
+}
