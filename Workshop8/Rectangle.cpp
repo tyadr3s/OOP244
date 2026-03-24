bbfc994a18d8090186c1b174022cd5343d73873e
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-03-24 19:23:43 +0000

    fix a bug

--- Workshop8/Rectangle.cpp
+++ Workshop8/Rectangle.cpp
@@ -32,11 +32,17 @@ namespace seneca {
    Rectangle::Rectangle(const char* label, int width, int height) : LblShape(label) {
       m_width = width;
       m_height = height;
-
-      if (m_height < 3) m_height = 3;
-
-      if (label() && m_width < (int)strlen(label()) + 2) {
-         m_width = (int)strlen(label()) + 2;
+      
+      if (m_height < 3) {
+         m_height = 3;
+      }
+      
+      const char* text = this->label();
+      
+      if (text != nullptr) {
+         if (m_width < (int)strlen(text) + 2) {
+            m_width = (int)strlen(text) + 2;
+         }
       }
    }
 
