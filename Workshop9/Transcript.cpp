9009ddcb6236147edc4f3cb15fd4c2ff5de1bae6
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-03-26 22:23:28 +0000

    Workshop 9 start and end

---
+++ Workshop9/Transcript.cpp
@@ -0,0 +1,58 @@
+/* Citation and Sources...
+-----------------------------------------------------------
+Workshop 9 
+Module: Transcript
+Filename: Transcript.cpp
+-----------------------------------------------------------
+Author: Carlos Andres Ramirez Moreno
+Student number: 120847256
+Email: caramirez-moreno@myseneca.ca
+Subject: OOP244 NBB
+-----------------------------------------------------------
+Revision History
+-----------------------------------------------------------
+Version Date      Reason
+1.0     2026/03/26  Implementation of Transcript.cpp
+-----------------------------------------------------------
+I have done all the coding by myself and only used the code that my professor provided to complete this workshop.
+*/
+
+#include <iostream>
+#include "Transcript.h"
+#include "Utils.h"
+using namespace std;
+namespace seneca {
+
+    Transcript::Transcript(const char* studentName, unsigned int studentNumber) : Marks() {
+        m_studentName = nullptr;
+        m_studentNumber = studentNumber;
+        ut.alocpy(m_studentName, studentName);
+    }
+    
+    Transcript::Transcript(const Transcript& other) : Marks(other) {
+        m_studentName = nullptr;
+        m_studentNumber = other.m_studentNumber;
+        ut.alocpy(m_studentName, other.m_studentName);
+    }
+        
+    Transcript& Transcript::operator=(const Transcript& other) {
+        if (this != &other) {
+            Marks::operator=(other);
+            ut.alocpy(m_studentName, other.m_studentName);
+            m_studentNumber = other.m_studentNumber;
+        }
+        return *this;
+    }
+    
+    ostream& Transcript::display(ostream& os) const {
+        os << m_studentName << " (" << m_studentNumber << ")" << endl;
+        os << "----------------------------------------------------------------------------" << endl;
+        Marks::display(os);
+        return os;
+    }
+    
+    Transcript::~Transcript() {
+        delete[] m_studentName;
+        m_studentName = nullptr;
+    }
+}
--- Workshop9/Transcript.h
+++ Workshop9/Transcript.h
@@ -2,15 +2,17 @@
 // OOP244 workshop 9, Transcript module
 //
 // File	Transcript.h
-// Version 1.0
+// Version 1.1
 // started by Fardad
 // Description
 //
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
+// Carlos Ramirez  2026/03/26      Completed Transcript class
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
+
 #ifndef SENECA_TRANSCRIPT_H
 #define SENECA_TRANSCRIPT_H
 #include <iostream>
@@ -19,15 +21,20 @@ namespace seneca {
    class Transcript :
       public Marks {
       // character pointer student name
-      
+      char* m_studentName;
       // unsigned integer or size_t student number
+      unsigned int m_studentNumber;
 
    public:
       // Constructor
-
+      Transcript(const char* studentName = nullptr, unsigned int studentNumber = 0);
       // Rule of Three
-
+      Transcript(const Transcript& other);
+      Transcript& operator=(const Transcript& other);
       // Destructor
+      ~Transcript();
+
+      std::ostream& display(std::ostream& os = std::cout) const;
    };
 }
 #endif // !SENECA_TRANSCRIPT_H
