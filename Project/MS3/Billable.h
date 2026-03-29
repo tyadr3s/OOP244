36662ba624f4ba88edcf96f642a6f5beac673a7f
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-03-29 16:37:03 -0400

    Update Billable.h

--- Project/MS3/Billable.h
+++ Project/MS3/Billable.h
@@ -1,3 +1,18 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: Billable
+Filename: Billable.h
+Version 1.0
+Author: Carlos Andres Ramirez Moreno, StNo: 120847256, Email: caramirez-moreno@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2026/03/29  Create billable and operators
+-----------------------------------------------------------
+I have done all the coding by myself and only used the project instructions
+and course materials provided by my professor.
+-----------------------------------------------------------*/
+
 #ifndef SENECA_BILLABLE_H
 #define SENECA_BILLABLE_H
 
