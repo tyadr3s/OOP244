3ada1cb4336054c82cd0a33be4e7c68cffa5138b
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-03-29 17:19:28 -0400

    Update Food.cpp

--- Project/MS3/Food.cpp
+++ Project/MS3/Food.cpp
@@ -1,3 +1,18 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: Billable
+Filename: Billable.cpp
+Version 1.0
+Author: Carlos Andres Ramirez Moreno, StNo: 120847256, Email: caramirez-moreno@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2026/03/29  Implemented billable metods and operators
+-----------------------------------------------------------
+I have done all the coding by myself and only used the project instructions
+and course materials provided by my professor.
+-----------------------------------------------------------*/
+
 #include <iostream>
 #include <iomanip>
 #include "Food.h"
