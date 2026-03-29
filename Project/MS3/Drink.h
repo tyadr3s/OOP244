96638fab001ff443c477bc0bad2dbb74e8304a71
Carlos Andres Ramirez Moreno <tyadr3s@gmail.com>
2026-03-29 17:23:24 -0400

    Update Drink.h

--- Project/MS3/Drink.h
+++ Project/MS3/Drink.h
@@ -1,3 +1,18 @@
+/* Citation and Sources...
+Final Project Milestone 3
+Module: Drink
+Filename: Drink.h
+Version 1.0
+Author: Carlos Andres Ramirez Moreno, StNo: 120847256, Email: caramirez-moreno@myseneca.ca
+Revision History
+-----------------------------------------------------------
+Date      Reason
+2026/03/29  Created all the Drink metods 
+-----------------------------------------------------------
+I have done all the coding by myself and only used the project instructions
+and course materials provided by my professor.
+-----------------------------------------------------------*/
+
 #ifndef SENECA_DRINK_H
 #define SENECA_DRINK_H
 
